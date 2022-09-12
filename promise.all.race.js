fcc6a0d340cad8217fd0c8d9edf663583f03d49f
paullaster <paullasterokoth98@gmail.com>
2022-09-12 10:19:42 +0300

    promise dot race condition

--- promise.all.race.js
+++ promise.all.race.js
@@ -4,11 +4,15 @@
  */
 
 const logins = new Promise ((resolve, reject) => {
-    resolve({username: "admin", password: "password"});
+    setTimeout(() => {
+        resolve({username: "admin", password: "password"});
+    },500);
 });
 
 const employeeInfo = new Promise((resolve, reject) => {
-    resolve({Dept: "Software ", position: "Software Engineer", name: "Paullaster Okoth"});
+    setTimeout(() => {
+        resolve({Dept: "Software ", position: "Software Engineer", name: "Paullaster Okoth"});
+    },400)
 });
 /*logins.then((data)=>{
     console.log(data)
@@ -17,7 +21,16 @@ employeeInfo.then((data)=>{
     console.log(data);
 });
 */
+
 Promise.all([logins, employeeInfo])
+.then((data)=>{
+    console.log(data);
+})
+.catch((err)=>{
+    console.log(err);
+});
+
+Promise.race([logins, employeeInfo])
 .then((data)=>{
     console.log(data);
 })
