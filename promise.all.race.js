ae2d112ca571e45bbfed49cfe021c3b8e232efa7
paullaster <paullasterokoth98@gmail.com>
2022-09-12 10:04:27 +0300

    promise dot all

---
+++ promise.all.race.js
@@ -0,0 +1,22 @@
+/**
+ * Promise.all returns an array if all the promises are resolved
+ * or Error if any promise is rejected
+ */
+
+const logins = new Promise ((resolve, reject) => {
+    resolve({username: "admin", password: "password"});
+});
+
+const employeeInfo = new Promise((resolve, reject) => {
+    resolve({Dept: "Software ", position: "Software Engineer", name: "Paullaster Okoth"});
+});
+/*logins.then((data)=>{
+    console.log(data)
+})
+employeeInfo.then((data)=>{
+    console.log(data);
+});
+*/
+Promise.all([logins, employeeInfo]).then((data)=>{
+    console.log(data);
+});
