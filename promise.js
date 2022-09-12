d6828ff09b5d7627fd2feeb9df8bede21400077f
paullaster <paullasterokoth98@gmail.com>
2022-09-12 06:08:51 +0300

    promises in action

--- promise.js
+++ promise.js
@@ -9,8 +9,25 @@ const myPromise = new Promise( (resolve,reject)=>{
         resolve("I am a promise");
     }, 1500)
 });
-myPromise.then((data)=>{
+/*myPromise.then((data)=>{
     console.log(data);
 }).then(()=>{
     console.log("I am an output stream after promise is resolved");
-}).catch((err)=>{   console.log(err)})
+}).catch((err)=>{   console.log(err)})*/
+
+/**
+ * Promises in action
+ */
+
+function adder(num1,num2){
+    return new Promise( (resolve,reject)=>{
+        const sumed = num1 + num2;
+        setTimeout(()=>{
+            resolve(sumed);
+        }, 1500)
+    })
+}
+
+adder(10,2).then((data)=>{
+    console.log(data);
+})
